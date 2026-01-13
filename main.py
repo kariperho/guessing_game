74180979a42ddf01bcd7bcd80891ef4424e2cb38
harrikmikkola <harri.k.mikkola@gmail.com>
2026-01-13 13:19:41 +0200

    Create main_game_loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class NumberGuessingGame:
 
-    def __init__(self):
-        pass  
+    def __init__(self, guess_count):
+        self.guess_count = guess_count
        
     def generate_random_number(self):
         pass 
@@ -13,8 +13,21 @@ class NumberGuessingGame:
         pass  
            
     def main_game_loop(self):
-        pass
+        while True:
+            correct_answer = generate_random_number()
+            player_answer = ask_number()
+            if correct_answer == player_answer:
+                print(f"Correct answer!! It was {correct_answer}")
+                break
+            elseif correct_answer < player_answer:
+                print(f"Wrong! {player_answer} is too high. Try again.")
+                add_guess_count()
+            elseif correct_answer > player_answer:
+                print(f"Wrong! {player_answer} is too low. Try again.")
+                add_guess_count
+        
 
 game = NumberGuessingGame()
+
 
 game.main_game_loop()
