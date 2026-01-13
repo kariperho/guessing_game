97edcc3f1f05cb9da5a1cda1ac775e5c59a40065
Kari Perho <151856768+kariperho@users.noreply.github.com>
2026-01-13 13:52:51 +0200

    Update main.py
    Fixed game logic with BRUTE FORCE

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ class NumberGuessingGame:
       
     def ask_number(self):
         number_guessed = input("What number do you want to guess?")
-        return number_guessed
+        return int(number_guessed)
               
     def add_guess_count(self):
         self.guess_count += 1
            
     def main_game_loop(self):
+        correct_answer = self.generate_random_number()
         while True:
-            correct_answer = generate_random_number()
-            player_answer = ask_number()
+            player_answer = self.ask_number()
             if correct_answer == player_answer:
                 print(f"Correct answer!! It was {correct_answer}")
                 print(f"It took you {self.guess_count} tries.")
                 break
             elif correct_answer < player_answer:
                 print(f"Wrong! {player_answer} is too high. Try again.")
-                add_guess_count()
+                self.add_guess_count()
             elif correct_answer > player_answer:
                 print(f"Wrong! {player_answer} is too low. Try again.")
-                add_guess_count()
+                self.add_guess_count()
         
 print("Let's play a game!")
 print("I'm thinking of a number between 1 and 10.")
@@ -37,6 +37,7 @@ print("Try to guess the number!")
 game = NumberGuessingGame()
 
 game.main_game_loop()
+
 
 
 
