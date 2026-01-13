0ec27a63822fbb33f2d220404998464e3fe1f915
Kari Perho <151856768+kariperho@users.noreply.github.com>
2026-01-13 13:08:02 +0200

    Added main.py
    Added the main game file

---
+++ main.py
@@ -0,0 +1,20 @@
+class NumberGuessingGame:
+
+    def __init__(self):
+        pass  
+       
+    def generate_random_number(self):
+        pass 
+      
+    def ask_number(self):
+        pass 
+              
+    def add_guess_count(self):
+        pass  
+           
+    def main_game_loop(self):
+        pass
+
+game = NumberGuessingGame()
+
+game.main_game_loop()
